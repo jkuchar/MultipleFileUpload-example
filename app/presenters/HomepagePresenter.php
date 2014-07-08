9f53483012ae84e7fee6ee91d5da45b6423707a1
Jan Kuchař <honza.kuchar@grifart.cz>
2014-07-08 17:51:11 +0200

    - removed Czech comments and messages

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -1,5 +1,7 @@
 <?php
 
+use Nette\Application\UI\Form;
+
 /**
  * My Application
  *
@@ -57,35 +59,34 @@ class HomepagePresenter extends BasePresenter {
 	}
 	
 	public function createComponentForm($name) {
-		$form = new Nette\Application\UI\Form($this, $name);
+		$form = new Form($this, $name);
 		$form->getElementPrototype()->class[] = "ajax";
 
-		/*$form->addText("test","Textové políčko")
-			->addRule(Form::FILLED, "Textové políčko test musí být vyplněno!");*/
+		$form->addText("textField0","Text field")
+			->addRule(Form::FILLED, "This is required text field.");
 
-		// Uploadů můžete do formuláře samozdřejmě přidat více, ale zatím je docela nepříjemná validace a jedna chybka v JS
 		$form->addMultipleFileUpload("upload","Attachments");
-			//->addRule("MultipleFileUpload::validateFilled","Musíte odeslat alespoň jeden soubor!")
-			//->addRule("MultipleFileUpload::validateFileSize","Soubory jsou dohromady moc veliké!",100*1024);
-		//$form->addMultipleFileUpload("upload2","Druhý balíček souborů");
+			//->addRule("MultipleFileUpload::validateFilled","You have to upload at least one file!")
+			//->addRule("MultipleFileUpload::validateFileSize","Files together are too large.",100*1024);
+		//$form->addMultipleFileUpload("upload2","Second file uploader");
 
-		$form->addSubmit("odeslat", "Odeslat");
-		$form->onSuccess[] = array($this,"zpracujFormular");
+		$form->addSubmit("send", "Submit yor form!");
+		$form->onSuccess[] = $this->handleFormSuccess;
 		
 		// Invalidace snippetů
 		$form->onError[] = array($this,"handleRedrawForm");
 		$form->onSuccess[] = array($this,"handleRedrawForm");
 	}
 
-	public function zpracujFormular(\Nette\Application\UI\Form $form) {
+	public function handleFormSuccess(Form $form) {
 		$data = $form->getValues();
 
-		// Předáme data do šablony
+		// Let's pass our data to template
 		$this->template->values = $data;
 
 		$queueId = uniqid();
 
-		// Přesumene uploadované soubory
+		// Moving uploaded files
 		foreach($data["upload"] AS $file) {
 			// $file je instance HttpUploadedFile
 			$newFilePath = \Nette\Environment::expand("%appDir%")."/../uploadedFilesDemo/q{".$queueId."}__f{".rand(10,99)."}__".$file->getName();
@@ -93,14 +94,17 @@ class HomepagePresenter extends BasePresenter {
 			// V produkčním módu nepřesunujeme soubory...
 			if(!\Nette\Environment::isProduction()) {
 				if($file->move($newFilePath))
-					$this->flashMessage("Soubor ".$file->getName() . " byl úspěšně přesunut!");
+					$this->flashMessage("File ".$file->getName() . " was successfully moved!");
 				else
-					$this->flashMessage("Při přesouvání souboru ".$file->getName() . " nastala chyba! Pro více informací se podívejte do logů.");
+					$this->flashMessage("Error while moving file ".$file->getName() . ".");
 			}
 		}
 	}
 
 	public function handleRedrawForm() {
+		// This invalidates snippet
+		// on AJAX requests this causes redrawing of the form
+		
 		$this->invalidateControl("form");
 	}
 
